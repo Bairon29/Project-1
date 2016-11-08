1a9ac5fc974eb269ed609dcbd7b461a2c3fa9d3b
Bairon29 <baironvasquez29@gmail.com>
2016-11-07 21:10:39 -0500

    update game to have win/lose logic, button, and wireframes

--- shooter.js
+++ shooter.js
@@ -4,6 +4,7 @@ console.log("loeaded");
   $(document).ready(function() {
     //global variable
 var index1 = 1;
+var active = false;
 var interval;
 var $body = $('body');
 var bulletOdj = new Object();
@@ -38,7 +39,7 @@ name: $('.player'),
 
 var bowser =  {
 name: $('#boss'),
-  posX: 800,
+  posX: 750,
   posY: 440,
   width1: 300,
   height1: 260,
@@ -52,10 +53,11 @@ this.posY = y;
 this.width1 = w;
 this.height1 = h;
 this.dimension = dimensionFunc;
+//this.burned = fireCollision;
 }
 
 var initialize = function(){
-this.name = ''
+this.name = '';
 this.posX = 0;
 this.posY = 0;
 this.width1 = 0;
@@ -105,28 +107,33 @@ thing.name.removeClass('form4');
 
 var stop11 = setInterval(function(){
 thing.name.toggleClass('final');
-thing.posX-=6;
+thing.posX-=10;
 thing.name.css('left', thing.posX);
 thing.dimension();
 
-if((thing.posX < player.posX + player.width1 - 60
-  && thing.posX + thing.width1 - 60 > player.posX
-  && thing.posY < player.posY + player.height1 - 60
-  && thing.posY + thing.height1 - 60 > player.posY)){
+if((thing.posX < player.posX + player.width1 - 20
+  && thing.posX + thing.width1 - 20 > player.posX
+  && thing.posY < player.posY + player.height1
+  && thing.posY + thing.height1 > player.posY)){
  clearInterval(stop11);
   var $currentplayer = $('.player');
   $currentplayer.remove();
   $('#mainContainer').html("");
-  $('#mainContainer').css('background', "url('images/cellplatform.jpg')");
+  $('#mainContainer').css('background', "url('images/gameover.jpg')");
+  $('#mainContainer').css('background-repeat', "no-repeat");
+  $('#mainContainer').css('background-size', "cover");
+  $('#try').css('opacity', '1');
   thing.name.remove();
   thing = new initialize();
 }
-else if(bulletCollision(thing)){
+else if(bulletCollision(thing, -10)){
   if(thing.name.children('.HIT').length === 3){
   clearInterval(stop11);
  thing.name.remove();
  thing = new initialize();
-  bigBoss();
+ if($('.zombieFinal').length === 0){
+   setTimeout(bigBoss, 3000);
+ }
 }
 }
 
@@ -166,30 +173,36 @@ thing.name.removeClass('form4');
 
 var stop1 = setInterval(function(){
 thing.name.toggleClass('final');
-thing.posX+=6;
+thing.posX+=10;
 thing.name.css('left', thing.posX);
 thing.dimension();
 
-if((thing.posX < player.posX + player.width1 - 60
-  && thing.posX + thing.width1 - 60 > player.posX
-  && thing.posY < player.posY + player.height1 - 60
-  && thing.posY + thing.height1 - 60 > player.posY)){
+if((thing.posX < player.posX + player.width1 - 20
+  && thing.posX + thing.width1 - 20 > player.posX
+  && thing.posY < player.posY + player.height1
+  && thing.posY + thing.height1  > player.posY)){
  clearInterval(stop1);
   var $currentplayer = $('.player');
   $currentplayer.remove();
   $('#mainContainer').html("");
-  $('#mainContainer').css('background', "url('images/cellplatform.jpg')");
+  $('#mainContainer').css('background', "url('images/gameover.jpg')");
+    $('#mainContainer').css('background-repeat', "no-repeat");
+  $('#mainContainer').css('background-size', "cover");
+  $('#try').css('opacity', '1');
   thing.name.remove();
   thing = new initialize();
 
 }
-else if(bulletCollision(thing)){
+else if(bulletCollision(thing, -10)){
 
   if(thing.name.children('.HIT').length === 3){
  clearInterval(stop1);
  thing.name.remove();
  thing = new initialize();
- bigBoss();}
+   if($('.zombieFinal').length === 0){
+   setTimeout(bigBoss, 3000);
+ }
+  }
 }
 
 }, 500);
@@ -199,22 +212,88 @@ else if(bulletCollision(thing)){
   },500);
 }
 
+var threwFlame = function(fires){
+  setTimeout(function(){
+  var stop4 = setInterval(function(){
+    if(bowser.name === ''){clearInterval(stop4);}
+    if(bowser.name !== ''){
+      fires.name.css('opacity', 1);
 
+    fires.name.css({
+      'left': fires.posX-=10,
+      'top': fires.posY++
+    });
+  }
+    if(fires.dimension()){
+      clearInterval(stop4);
+      fires.name.remove();
+      fires = new initialize();
+      bigBoss();
+    }
+    else if(fireCollision(fires)){
+      if(player.name.children('.burn').length === 2){
+        clearInterval(stop4);
+  player.name.remove();
+  player = new initialize();
+      fires.name.remove();
+      fires = new initialize();
+       $('#mainContainer').html("");
+  $('#mainContainer').css('background', "url('images/gameover.jpg')");
+    $('#mainContainer').css('background-repeat', "no-repeat");
+  $('#mainContainer').css('background-size', "cover");
+  $('#try').css('opacity', '1');
+}else{
+      clearInterval(stop4);
+      fires.name.remove();
+      fires = new initialize();
+      bigBoss();}
+    }
+
+    },100);
+},3000);
+}
+
+var fireCollision = function(fireThrown){
+if((fireThrown.posX < player.posX + player.width1 - 10
+  && fireThrown.posX + fireThrown.width1 - 10 > player.posX
+  && fireThrown.posY < player.posY + player.height1
+  && fireThrown.posY + fireThrown.height1 > player.posY)){
+  player.name.append('<div class="burn" ></div>');
+
+  return true;
+}
+return false;
+}
 
 var bigBoss = function(){
-  setTimeout(function(){
+active = true;
 bowser.name.css('opacity', '1');
-  },5000)
+//bulletCollision(bowser);
+//var stop4 = setInterval(function(){
+   if(bowser.name !== ''){
+    var fire = new obj('<div id="fire" ></div>'
+      ,bowser.posX +60, bowser.posY +30, 100, 120);
+    var $container = $('#mainContainer');
+
+    $container.append(fire.name);
+    threwFlame(fire);}
+
+    // fire.name.css('opacity', 1);
+    // fire.name.css({
+    //   'left': fire.posX+=10,
+    //   'top': fire.posY++
+    // });
+//},5000);
 }
 
 
 
-var bulletCollision = function(currentEnemy){
+var bulletCollision = function(currentEnemy, off){
   if(Object.keys(bulletOdj).length > 0){
     console.log("enemies"+bulletOdj);
     for(var i in bulletOdj){
-  if((currentEnemy.posX < bulletOdj[i].posX + bulletOdj[i].width1 - 10
-  && currentEnemy.posX + currentEnemy.width1 - 10 > bulletOdj[i].posX
+  if((currentEnemy.posX < bulletOdj[i].posX + bulletOdj[i].width1 + off
+  && currentEnemy.posX + currentEnemy.width1 + off > bulletOdj[i].posX
   && currentEnemy.posY < bulletOdj[i].posY + bulletOdj[i].height1
   && currentEnemy.posY + currentEnemy.height1> bulletOdj[i].posY)){
     currentEnemy.name.append('<p class="HIT" ></p>');
@@ -234,12 +313,8 @@ var fireBulletRight = function(power){
 var currenBullet = new obj('<div class='+power+'></div>', player.posX+player.width1,
   player.posY + 17, 73, 50);
 currenBullet.name.removeAttr('id');
-if(Object.keys(bulletOdj).length === 0){
-bulletOdj[Object.keys(bulletOdj).length] = currenBullet;}
-else{
- bulletOdj[Object.keys(bulletOdj).length -(Object.keys(bulletOdj).length - 1)] = currenBullet;
 
-}
+bulletOdj[Object.keys(bulletOdj).length] = currenBullet;
 
 var $container = $('#mainContainer');
 $container.append(currenBullet.name);
@@ -249,11 +324,24 @@ currenBullet.name.css({
 });
 $(document).keyup(function(event) {
   if(event.which === 90){
-
+$('#zKey').css('border', '0px');
+$('#zKey').css('box-shadow', '0px 0px 0px 0px rgba(0,0,0,0)');
   var stop = setInterval(function(){
     currenBullet.posX+=20;
     currenBullet.name.css('left', currenBullet.posX);
-
+    if(active === true){
+ if(bulletCollision(bowser, -150)){
+      if(bowser.name.children('.HIT').length === 8){
+        bowser.name.remove();
+        bowser = new initialize();
+        $('#mainContainer').html("");
+  $('#mainContainer').css('background', "url('images/win.png')");
+    $('#mainContainer').css('background-repeat', "no-repeat");
+  $('#mainContainer').css('background-size', "cover");
+    $('#try').css('opacity', '1');
+      }
+    }
+}
 
     if(currenBullet.dimension()){
       currenBullet.name.remove();
@@ -267,11 +355,9 @@ var fireBulletLeft = function(power){
 var currenBullet = new obj('<div class='+power+'></div>', player.posX-73,
   player.posY + 17, 73, 50);
 currenBullet.name.attr('id', 'player');
-if(Object.keys(bulletOdj).length === 0){
-bulletOdj[Object.keys(bulletOdj).length] = currenBullet;}
-else{
- bulletOdj[Object.keys(bulletOdj).length -(Object.keys(bulletOdj).length - 1)] = currenBullet;
-}
+
+bulletOdj[Object.keys(bulletOdj).length] = currenBullet;
+
 
 var $container = $('#mainContainer');
 $container.append(currenBullet.name);
@@ -281,15 +367,25 @@ currenBullet.name.css({
 });
 $(document).keyup(function(event) {
   if(event.which === 90){
-
+$('#zKey').css('border', '0px');
+$('#zKey').css('box-shadow', '0px 0px 0px 0px rgba(0,0,0,0)');
   var stop = setInterval(function(){
     currenBullet.posX-=20;
     currenBullet.name.css('left', currenBullet.posX);
-
+// debugger;
+    if(active === true){
+ if(bulletCollision(bowser, -150)){
+      if(bowser.name.children('.HIT').length === 8){
+        bowser.name.remove();
+        bowser = new initialize();
+      }
+    }
+}
 
 
     if(currenBullet.dimension()){
       currenBullet.name.remove();
+      // currenBullet = new initialize();
      clearInterval(stop);
     }
   },100);
@@ -303,14 +399,21 @@ $(document).keyup(function(event) {
 var movePlayer = function(){
   $(document).keydown(function(event) {
     if(event.which === 38){
+       $('#upKey').css('border', '3px solid white');
+    $('#upKey').css('box-shadow', '1px 2px 80px 30px black');
+      console.log(event.which);
       player.posY-=6;
       player.name.css('top', player.posY+'px');
     }
     if(event.which === 40){
+       $('#downKey').css('border', '3px solid white');
+    $('#downKey').css('box-shadow', '1px 2px 80px 30px black');
       player.posY+=6;
       player.name.css('top', player.posY+'px');
     }
     if(event.which === 39){
+       $('#rightKey').css('border', '3px solid white');
+    $('#rightKey').css('box-shadow', '1px 2px 80px 30px black');
         player.posX+=10;
         player.name.css('left', player.posX+'px');
         player.name.removeAttr('id');
@@ -318,18 +421,42 @@ var movePlayer = function(){
         walkAnimation();
     }
     if(event.which === 37){
+       $('#leftKey').css('border', '3px solid white');
+    $('#leftKey').css('box-shadow', '1px 2px 80px 30px black');
       player.posX-=10;
       player.name.css('left', player.posX+'px');
        player.name.attr('id', 'player');
        player.direction = 'left';
        walkAnimation();
     }
-// if((player.posX < player1.posX + player1.width1
-//   && player.posX + player.width1 > player1.posX
-//   && player.posY < player1.posY + player1.height1
-//   && player.posY + player.height1 > player1.posY)){
-//   console.log("touched");
-// }
+    if(active === true){
+if((player.posX < bowser.posX + bowser.width1 - 140
+  && player.posX + player.width1 - 140 > bowser.posX
+  && player.posY < bowser.posY + bowser.height1
+  && player.posY + player.height1 > bowser.posY)){
+  player.name.remove();
+  player = new initialize();
+}}
+
+$(document).keyup(function(event) {
+  if(event.which === 38){
+      $('#upKey').css('border', '0px');
+      $('#upKey').css('box-shadow', '0px 0px 0px 0px rgba(0,0,0,0)');
+    }
+    if(event.which === 40){
+      $('#downKey').css('border', '0px');
+      $('#downKey').css('box-shadow', '0px 0px 0px 0px rgba(0,0,0,0)');
+    }
+    if(event.which === 39){
+        $('#rightKey').css('border', '0px');
+        $('#rightKey').css('box-shadow', '0px 0px 0px 0px rgba(0,0,0,0)');
+    }
+    if(event.which === 37){
+       $('#leftKey').css('border', '0px');
+       $('#leftKey').css('box-shadow', '0px 0px 0px 0px rgba(0,0,0,0)');
+    }
+});
+
 player.dimension();
   });
 }
@@ -349,14 +476,36 @@ var stop =  setInterval(function(){
 }
   });
 },500)
+
+$('#gameStory').click(function(event) {
+  $('#game').css('opacity', '1');
+  $('#user').css('opacity', '0');
+});
+
+$('#userStory').click(function(event) {
+  $('#user').css('opacity', '1');
+  $('#game').css('opacity', '0');
+});
+
 $(document).keydown(function(event) {
   console.log(event.which);
     if(event.which === 32){
+      $('#gameStory').css('opacity', '0');
+      $('#userStory').css('opacity', '0');
+      $('#game').css('opacity', '0');
+      $('#user').css('opacity', '0');
+
+      $('#zKey').css('opacity', '1');
+      $('#upKey').css('opacity', '1');
+      $('#rightKey').css('opacity', '1');
+      $('#leftKey').css('opacity', '1');
+      $('#downKey').css('opacity', '1');
+
       var stop2 = setInterval(function(){
-        if(amount < 1){
+        if(amount < 6){
   amount++;
 zombieGoingLeft('left');
-//zombieGoingRight('right');
+zombieGoingRight('right');
 
 }
 else{ clearInterval(stop2);
@@ -368,6 +517,8 @@ else{ clearInterval(stop2);
   if(player.direction === 'right'){
 
   if(event.which === 90){
+    $('#zKey').css('border', '3px solid white');
+    $('#zKey').css('box-shadow', '1px 2px 80px 30px black');
   fireBulletRight('bullet');
 }
 }
@@ -375,6 +526,8 @@ else{ clearInterval(stop2);
  else if(player.direction === 'left'){
 
   if(event.which === 90){
+    $('#zKey').css('border', '3px solid white');
+    $('#zKey').css('box-shadow', '1px 2px 80px 30px black');
   fireBulletLeft('bullet');
 }
 
